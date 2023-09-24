febb1ca58d545bc73c3b4ccbff778a82078123eb
trungvuive <trung150502@gmail.com>
2023-09-24 23:03:06 +0700

    add comment form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,25 @@
 
                     You are logged in!
                 </div>
+
+                <section class="container">
+                    <h5>{{ thoughtStatus }}</h5>
+                    <form action="" method="POST">
+                        <label for="thought">What's on your mind?</label>
+                        <textarea id="thought" name="thought" rows="5" cols="10"></textarea>
+                        <label for="name">And who you are?(Optional):</label>
+                        <input type="text" id="name" name="name">
+                    </form>
+                </section>
+
+                <section class="container">
+                    <h4>Photo Gallery</h4>
+                    <div class="row">
+                        <div class="col">
+                            Hehehe
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
     </div>
