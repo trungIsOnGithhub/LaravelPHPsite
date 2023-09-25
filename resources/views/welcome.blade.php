fcd3f9021de6b388673d13e3612d1a792576d4b3
trung150502 <trung150502@gmail.com>
2023-09-25 21:47:16 +0700

    remove test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,49 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo("abcd") ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="card-header">Dashboard</div>
 
-        <title>Trung Dep Zai</title>
+    <section class="container justify-content-center">
+        <h5>qhudalw87319</h5>
+        <form action="/thought" method="POST">
+            <label for="thought">What's on your mind?</label>
+            <textarea id="thought" name="thought" rows="5" cols="10"></textarea>
+            <label for="name">And who you are?(Optional):</label>
+            <input type="text" id="name" name="name">
+            <button type="submit">Submit</button>
+        </form>
+    </section>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Trung Dep Zai
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+    <section class="container  justify-content-center">
+        <h4>Photo Gallery</h4>
+        <div class="row">
+            <div class="col-xs-12 col-md-4">
+                <img src="meotom.jpeg" alt="Meo Tom" width="200" height="300">
+            </div>
+            <div class="col-xs-12 col-md-4">
+                <img src="meotom.jpeg" alt="Meo Tom" width="200" height="300">
+            </div>
+            <div class="col-xs-12 col-md-4">
+                <img src="meotom.jpeg" alt="Meo Tom" width="200" height="300">
             </div>
         </div>
-    </body>
+        <div class="row">
+            <div class="col-xs-12 col-md-4">
+                <img src="meotom.jpeg" alt="Meo Tom" width="200" height="300">
+            </div>
+            <div class="col-xs-12 col-md-4">
+                <img src="meotom.jpeg" alt="Meo Tom" width="200" height="300">
+            </div>
+            <div class="col-xs-12 col-md-4">
+                <img src="meotom.jpeg" alt="Meo Tom" width="200" height="300">
+            </div>
+        </div>
+    </section>
+<body>
 </html>
