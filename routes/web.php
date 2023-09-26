aa9c0686bbb3373d7e5424f34090411200d6c457
trung <trung@gmail.com>
2023-09-26 21:46:48 +0700

    remove auth

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::post('/thought', 'ThoughtController@postOneThought');
 
 Route::get('/home', 'HomeController@index')->name('home');
