de99bfbc14734b9a03c44fcf685d5f5f2d84125a
Trung Nguyen <trung150502@gmail.com>
2023-10-05 08:51:18 +0000

    add new migration

---
+++ database/migrations/2023_10_05_073702_create_thought_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThoughtTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('thought', function (Blueprint $table) {
+            // for MySQL DB Only
+            if( !Schema::hasTable('thought') ) {
+                Schema::drop('thought');
+            }
+
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+            $table->engine = 'InnoDB';
+
+            $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->string('content', 65535);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('thought');
+    }
+}
